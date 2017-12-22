dd705678fe3a0d8ac82d88ab181f6f70c9ae7b99
赵子豪 <13969918264@163.com>
2017-12-22 09:16:31 +0800

    Update test_file_generator.c

--- test_file_generator.c
+++ test_file_generator.c
@@ -2,7 +2,7 @@
 	Name: 赵子豪(ZiHao Zhao)
 	Date: 2017年12月16日
 */
-/*	test_file_generator.c -- generate pseudonumbers by calling the standard library and for sort	*/
+/*	test_file_generator.c -- generate pseudonumbers by calling the standard library	\*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,11 +10,10 @@
 
 int main(void)
 {
-	long long int i;
+	long int i;
 
 	for (i = 0; i < MAX; i++)
-		printf("%ld\n", rand() * rand() * rand());
+		printf("%ld\n", (long)((long)rand() * (long)rand() * (long)rand()));
 
 	return 0;
-
 }
