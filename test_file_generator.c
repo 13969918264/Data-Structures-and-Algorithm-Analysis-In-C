d89509a68c9e0ae6808606dc605f8c92c3f93efa
赵子豪 <13969918264@163.com>
2017-12-25 16:18:47 +0800

    Update test_file_generator.c

--- test_file_generator.c
+++ test_file_generator.c
@@ -2,7 +2,8 @@
 	Name: 赵子豪(ZiHao Zhao)
 	Date: 2017年12月16日
 */
-/*	test_file_generator.c -- generate pseudonumbers by calling the standard library	\*/
+/*	test_file_generator.c -- generate pseudonumbers by calling the standard library	*/
+/*	You have to use redirection	*/
 #include <stdio.h>
 #include <stdlib.h>
 
